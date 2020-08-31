4a7583d5f7eef22b3aa74487b46028aa8b5c92ad
Korver Huang <korver2017@gmail.com>
2020-08-31 21:31:12 +0800

    Initialize MySQL

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let express = require ('express');
 let app = express ();
+var path = require ('path');
+var mysql = require ('mysql');
 
 app.get ('/', function (req, res) {
   res.send ('Hello World!');
@@ -7,4 +9,33 @@ app.get ('/', function (req, res) {
 
 app.listen (3000, function () {
   console.log ('Example app listening on port 3000!');
+});
+
+var con = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'password',
+	database: 'demo_nodejs',
+	port: 3306
+});
+
+con.connect (function (err) {
+
+  if (err) {
+		console.log ('connecting error');
+		console.log (err);
+    return;
+  }
+
+  console.log ('connecting success');
+});
+
+con.end ();
+
+app.use (express.static (path.join (__dirname, 'public')));
+
+// DB state
+app.use (function (req, res, next) {
+  req.con = con;
+  next();
 });
