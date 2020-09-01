cb7101650a17f9059c2bba28f4f062a955aaa801
Korver Huang <korver2017@gmail.com>
2020-09-01 13:00:58 +0800

    Add routes & render pug

--- index.js
+++ index.js
@@ -1,46 +1,18 @@
 let express = require ('express');
 let app = express ();
-var path = require ('path');
-var mysql = require ('mysql');
-
-app.get ('/', function (req, res) {
-  res.send ('Hello World!');
-});
+var wiki = require('./routes/wiki.js');
 
 app.listen (3000, function () {
   console.log ('Example app listening on port 3000!');
 });
 
-var con = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: 'password',
-	database: 'sitepoint',
-	port: 3306
+app.use('/wiki', wiki);
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get('/', function (req, res) {
+  
+  
+  res.render('index', { title: 'Hey', message: 'Hello there!' });
 });
-
-con.connect (function (err) {
-
-  if (err) {
-		console.log ('connecting error');
-		console.log (err);
-    return;
-  }
-
-  console.log ('connecting success');
-});
-
-con.query ('SELECT * FROM authors', (err, rows) => {
-  if (err) throw err;
-
-  console.log ('Data received from Db:');
-  console.log (rows);
-});
-
-con.query ('SELECT 12 + 34 As result', (err, rows) => {
-  if (err) throw err;
-
-	console.log ('The result is: ', rows[0].result);
-});
-
-con.end ();
