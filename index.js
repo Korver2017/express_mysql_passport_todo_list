8ab1459cd3278ea49d8940ee203a0239c42cac63
Korver Huang <korver2017@gmail.com>
2020-09-14 10:24:29 +0800

    Add delete item method & trim code

--- index.js
+++ index.js
@@ -1,18 +1,66 @@
 let express = require ('express');
 let app = express ();
-var wiki = require('./routes/wiki.js');
+let mysql = require ('mysql');
+let cors = require ('cors');
+
+app.use (cors ());
 
 app.listen (3000, function () {
   console.log ('Example app listening on port 3000!');
 });
 
-app.use('/wiki', wiki);
+app.use (express.json ());
 
-app.set('views', './views');
-app.set('view engine', 'pug');
-
-app.get('/', function (req, res) {
-  
-  
-  res.render('index', { title: 'Hey', message: 'Hello there!' });
+var con = mysql.createConnection ({
+	host: 'localhost',
+	user: 'root',
+	password: 'password',
+	database: 'sitepoint',
+	port: 3306
 });
+
+con.connect (function (err) {
+
+  if (err) {
+		console.log ('connecting error');
+		console.log (err);
+    return;
+  }
+
+  console.log ('connecting success');
+});
+
+app.get ('/', function (req, res) {
+
+  con.query ('SELECT * FROM todo_list', (err, rows) => {
+
+    if (err)
+      throw err;
+
+    console.log ('Data received from Db:');
+    console.log (rows);
+
+    let data = rows;
+
+    res.send (data);
+  });
+});
+
+app.post ('/remove', function (req, res) {
+
+  console.log (req.body.todo_id);
+
+  let id = req.body.todo_id;
+
+  con.query (`DELETE FROM todo_list WHERE todo_id = '${id}'`, function (error, results, fields) {
+
+      if (error)
+        throw error;
+
+      else 
+        res.json ({error: false, data: results, message: 'Deleted sucessfully.'});
+        // return res.send({ error: false, data: results, message: 'products delete.' });
+  });
+});
+
+console.log ('End');
